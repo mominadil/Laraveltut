55a0a9fa65138eaadae905ccc687f4a6e1223916
mmlteam <support@makemelive.in>
2021-12-20 11:25:06 +0530

    unless

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -9,8 +9,26 @@
  @if($post['is_new'])
  <div>A new blog post! Using if  </div>
  @else(!$post['is_new'])
- <div>Blog post is old! using elseif</div>
+ <div>Blog post is old! using elseif <br></div>
  @endif
+@unless ($post['is_new'])
+<div>It is an old post ... using unless <br></div>
+@endunless
+
+
+
+
  <h1>{{ $post['title'] }}</h1>
  <p>{{ $post['content'] }}</p>   
+
+@isset($post['has_comments'])
+<div>The post has some comment.... using isset</div>
+
+@endisset
+
+
+@for ($i = 0; $i < 10; $i++)
+<div>  The current value is {{ $i }} <br><br></div>
+@endfor
+
 @endsection
