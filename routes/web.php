5e22474b0ff9480555ca3846a2b524b7d61f4a6c
mmlteam <support@makemelive.in>
2021-12-21 15:18:16 +0530

    Added routes and redirect methods

--- routes/web.php
+++ routes/web.php
@@ -80,3 +80,18 @@ Route::get('/fun/responses',function() use($posts){
 
 });
 
+Route::get('/fun/redirect',function(){
+    return redirect('/contact');
+});
+
+Route::get('/fun/back',function(){
+    return back();
+});
+
+Route::get('/fun/named-route',function(){
+    return redirect()->route('post.show',['id'=>1]);
+});
+
+Route::get('/fun/away',function(){
+    return redirect()->away('https://google.com');
+});
